491966f16dddd5f9a75fa05043b982d1688beafe
Marine PC <marinecp27@yahoo.fr>
2020-11-02 15:45:25 +0100

    maj index.js

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
 'use strict'
 
 var express = require('express');
-var fetch = require('node-fetch');
-var https = require('https');
-
 var app = express();
 
+const port = process.env.PORT || 3000;
+
+var fetch = require('node-fetch');
+var https = require('https');
+var cors = require('cors');
+
+var corsOptions = {
+    origin: 'https://natoine.github.io',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+//serves static files
 app.use(express.static('docs'));
 
-const port = process.env.PORT || 3000 ;
 
-app.get("/", function(req, res){
-    res.send("helloWorld !");
+//ROUTES
+
+app.get("/:name", function(req, res) {
+    res.send("hello : " + req.params.name);
 })
 
-app.get("/:name", function(req, res){
-    res.send("hello : " + req.params.name );
-})
-app.get("/fetchair/shangai", function(req, res){
-    let url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&rows=139&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes" ;
+app.get("/fetchair/shangai", cors(corsOptions), function(req, res) {
+    let url = "http://api.waqi.info/feed/shanghai/?token=demo";
     fetch(url)
-    .then(res => res.json())
-    .then(json => {
-        console.log("fetchair", json);
-        res.send("data fetched look your console");
-    });
+        .then(res => res.json())
+        .then(json => {
+            console.log("fetchair", json);
+            res.send("data fetched look your console");
+        });
 })
-app.get("/requestair/shangai", function(req, res){
-    let url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&rows=139&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes" ;
+
+app.get("/requestair/shangai", function(req, res) {
+    let url = "https://api.waqi.info/feed/shanghai/?token=demo";
     https.get(url, (resp) => {
         let data = '';
 
         // A chunk of data has been recieved.
         resp.on('data', (chunk) => {
             data += chunk;
-         });
+        });
         // The whole response has been received. Print out the result.
         resp.on('end', () => {
             console.log("requestair", JSON.parse(data));
@@ -47,8 +55,6 @@ app.get("/requestair/shangai", function(req, res){
     });
 })
 
-app.listen(port, function () {
+app.listen(port, function() {
     console.log('Serveur listening on port ' + port);
 });
-
-
