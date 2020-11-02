b881580802028a4ba9f3912774f182112a4e3614
acanetti <47980384+acanetti@users.noreply.github.com>
2020-11-02 12:10:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 const port = process.env.PORT || 3000 ;
 
 app.get("/", function(req, res){
-    res.sendFile('/index.html');
+    res.sendFile('index.html');
 });
 
 app.listen(port, function () {
