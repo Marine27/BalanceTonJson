c5f66fd9c085359acc0b9213e92e89ed3efebd30
acanetti <47980384+acanetti@users.noreply.github.com>
2020-11-02 12:03:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ var app = express();
 const port = process.env.PORT || 3000 ;
 
 app.get("/", function(req, res){
-    res.send("helloWorld !");
-})
+    res.sendFile(path.join(__dirname + '/index.html'));})
 
 app.listen(port, function () {
     console.log('Serveur listening on port ' + port);
