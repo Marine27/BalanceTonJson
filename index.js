8ea327134ba927a55d6da128baf823fd257044d9
Marine PC <marinecp27@yahoo.fr>
2020-11-03 09:58:53 +0100

    maj cors

--- index.js
+++ index.js
@@ -20,45 +20,50 @@ app.use(express.static('docs'));
 
 
 //ROUTES
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.send("helloWorld !");
 })
 
-app.get("/:name", function(req, res){
-    res.send("hello : " + req.params.name );
+/*
+app.get("/:name", function(req, res) {
+    res.send("hello : " + req.params.name);
 })
-app.get("/fetchair/velib", function(req, res){
+*/
 
-    let url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&rows=139&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes"  ;
-    fetch(url)
-        .then(res => res.json())
-        .then(json => {
-            console.log("fetchair", json);
-            res.send("data fetched look your console");
+app.get("/fetchair/velib", cors(corsOptions), function(req, res) {
+
+        let url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&rows=139&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes";
+        fetch(url)
+            .then(res => res.json())
+            .then(json => {
+                console.log("fetchair", json);
+                res.send("data fetched look your console");
+            });
+    })
+    /* v2 pour fetch
+    app.get("/requestair/velib", function(req, res) {
+
+        let url = velibJson;
+        https.get(url, (resp) => {
+            let data = '';
+
+            // A chunk of data has been recieved.
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+                console.log("requestair", JSON.parse(data));
+                res.send("data requested look your console");
+            });
+
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+            res.send("nope request didnt work");
         });
-}   )
-app.get("/requestair/velib", function(req, res){
+    })
+    */
 
-    let url = velibJson ;
-    https.get(url, (resp) => {
-        let data = '';
-
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-            data += chunk;
-        });
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-            console.log("requestair", JSON.parse(data));
-            res.send("data requested look your console");
-        });
-
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-        res.send("nope request didnt work");
-    });
-})
-
-app.listen(port, function () {
+app.listen(port, function() {
     console.log('Serveur listening on port ' + port);
-});
+})
