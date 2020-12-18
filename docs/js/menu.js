af00a323c0d1c035d4353944d1ba3451dfca7ced
Petrichoeur <florian.bobo34@gmail.com>
2020-12-18 13:53:44 +0100

    css quick fix

--- docs/js/menu.js
+++ docs/js/menu.js
@@ -8,8 +8,16 @@ function myFunction3() {
   document.getElementById("myDropdown3").classList.toggle("show");
 }
 function myFunction4() {
-  document.getElementById("myDropdown4").classList.toggle("show");
+  var x = document.getElementById("myDropdown4");
+  var x2 = document.getElementById("myDropdown5");
+    x.style.display = "block";
+    x2.style.display= "none"
+
 }
 function myFunction5() {
-  document.getElementById("myDropdown5").classList.toggle("show");
+  var x = document.getElementById("myDropdown4");
+  var x2 = document.getElementById("myDropdown5");
+    x2.style.display = "block";
+    x.style.display= "none"
+
 }
